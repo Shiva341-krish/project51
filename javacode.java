57ea596f3cbdc56eb06c99fe5f2c3b144a180996
Shiva341-krish <krish76shiva@gmail.com>
2025-04-08 19:07:17 +0530

    this is my project51

---
+++ javacode.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+
+    // Main method: the entry point of the program
+    public static void main(String[] args) {
+        // Print a simple message to the console
+        System.out.println("Hello, World!");
+        
+        // Example of variables and arithmetic operation
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
