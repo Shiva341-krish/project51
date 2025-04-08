2a0d5def200e25843be59b53ff1c3eeb494f767b
Shiva341-krish <krish76shiva@gmail.com>
2025-04-08 19:37:27 +0530

    Update javacode.java

--- javacode.java
+++ javacode.java
@@ -3,7 +3,7 @@ public class HelloWorld {
     // Main method: the entry point of the program
     public static void main(String[] args) {
         // Print a simple message to the console
-        System.out.println("Hello, Hi Everyone World-World!");
+        System.out.println("Hello, Hi Everyone World-World-World!");
         
         // Example of variables and arithmetic operation
         int a = 5;
